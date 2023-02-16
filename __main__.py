90b6544548d88c20ef4f3bc76f117c08c86b915c
gaillard <florian18.01gaillard@gmail.com>
2023-02-16 10:43:59 +0100

    create board plus plus

--- __main__.py
+++ __main__.py
@@ -15,7 +15,8 @@ def create_board_info(file):
                 board_info['map'] = {}
                 current_key = 'map'
             elif "ghosts:" in line:
-                current_key = "ghosts"
+                current_key = "spawn"
+                board_info['spawn'] = {}
             elif "magic:" in line:
                 current_key = "magic"
             elif current_key != "":
@@ -25,17 +26,16 @@ def create_board_info(file):
                 if current_key == 'map':
                     size_x, size_y = int(split_line[0]), int(split_line[1])
                     board_info['size'] = size_x, size_y
-                    for x in range(1, size_x + 1):
-                        for y in range(1, size_y + 1):
-                            board_info['map'][(x, y)] = 0
-                elif current_key == 'ghosts':
+                elif current_key == 'spawn':
                     team, x, y = int(split_line[0]), int(split_line[1]), int(split_line[2])
-                    board_info['map'][(x, y)] = team
-                    board_info[current_key].append(tuple(line.strip().split()))
+                    if team == 1:
+                        board_info[current_key]['team1'] = (x, y)
+                        board_info['map'][(x, y)] = 'team1'
+                    else:
+                        board_info[current_key]['team2'] = (x, y)
+                        board_info['map'][(x, y)] = 'team2'
                 else:
                     if current_key == 'magic':
-                        x, y, nb_magic = int(split_line[0]), int(split_line[1]), int(split_line[2])
-                        board_info['map'][(x, y)] += nb_magic
                         board_info[current_key].append(tuple(line.strip().split()))
     else:
         print("This file is not in .ght please put an other one.")
@@ -43,24 +43,44 @@ def create_board_info(file):
 
 
 def create_board(board_info):
+    row = 2
+    column = 4
     coord_y, coord_x = board_info['size']
-    for y in range((coord_y * 4) + 1):
-        for x in range((coord_x * 8) + 1):
+    for y in range((coord_y * row) + 1):
+        for x in range((coord_x * column) + 1):
             with term.location(x, y):
-                if x % 8 == 0 and y % 4 == 0:
-                    print('+')
-                elif x % 8 == 0 and y % 4 != 0:
-                    print('|')
-                elif y % 4 == 0 and x % 8 != 0:
-                    print('―')
-                    # Print coord x
-            if ((coord_y * 4) == y and x % 4 == 0) and x % 8 != 0:
+                if x == 0 and y == 0:
+                    print('┌')
+                elif x % (column * coord_x) == 0 and y == 0:
+                    print('┐')
+                elif y == 0 and x % column == 0:
+                    print('┬')
+                elif x == 0 and y % (row*coord_x) == 0:
+                    print('└')
+                elif x % (column*coord_x) == 0 and y % (row*coord_x) == 0:
+                    print('┘')
+                elif x % column == 0 and y % (row*coord_x) == 0:
+                    print('┴')
+                elif x == 0 and y % row == 0:
+                    print('├')
+                elif x % (column*coord_x) == 0 and y % row == 0:
+                    print('┤')
+                elif x % column == 0 and y % row == 0:
+                    print('┼')
+                elif x % column == 0 and y % row != 0:
+                    print('│')
+                elif y % row == 0 and x % column != 0:
+                    print('─')
+                    # Print coord Y
+            if ((coord_y * row) == y and x % (column // 2) == 0) and x % column != 0:
                 with term.location(x, y + 1):
-                    print((x + 4) // 8)
-                    # Print coord y
-            elif ((coord_x * 8) == x and y % 2 == 0) and y % 4 != 0:
+                    print((x + column // 2) // column)
+                    # Print coord X
+            elif ((coord_x * column) == x and y % (row // 2) == 0) and y % row != 0:
                 with term.location(x + 1, y):
-                    print((y + 2) // 4)
+                    print((y + row // 2) // row)
+
+
 
 
 def player_information(board_info):
@@ -68,8 +88,16 @@ def player_information(board_info):
 
 
 def main():
-    print(term.home + term.clear)
-    create_board(create_board_info('bruh.ght'))
+    game_over = False
+    board_info = create_board_info('bruh.ght')
+    with term.fullscreen():
+        while not game_over:
+            print(term.home + term.clear)
+            coord_y, coord_x = board_info['size']
+            create_board(board_info)
+            with term.location(0, coord_y * 8):
+                if input():
+                    game_over = True
 
 
 if __name__ == "__main__":
